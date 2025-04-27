4681d8bc819dc98720ed06e06188bf51f7b1dc0b
Debasishphukan <debasishphukan94@gmail.com>
2025-04-28 00:25:10 +0530

    inital 2

--- components/MeetingCard.tsx
+++ components/MeetingCard.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
-import { avatarImages } from "@/constants";
 import { useToast } from "./ui/use-toast";
 import { useState } from "react"; // 👈 ADD this import
 import axios from "axios";
